86e0c7dd1241d76d039692c115943a1233158269
JiggyStardust <saaraleni@gmail.com>
2025-05-28 15:22:06 +0300

    reading config_file and couting it without comments or unnecessary spaces

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include <algorithm>
 #include <map>
 
+#include "ConfigParser.cpp"
+
 int	mvp(void)
 {
 	int epoll_fd = epoll_create1(0);
@@ -68,15 +70,29 @@ int	mvp(void)
 	return 0;
 }
 
+// int	main(int argc, char **argv)
+// {
+// 	(void)argc;
+// 	(void)argv;
+// 	// if (argc != 2 || std::string(argv[1]) != "configuration/webserv.conf")
+// 	// {
+// 	// 	std::cout << "\nRun with \"./webserv configuration/webserv.conf\"" << std::endl;
+// 	// 	return (1);
+// 	// }
+// 	mvp();
+// 	return (0);
+// }
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	// if (argc != 2 || std::string(argv[1]) != "configuration/webserv.conf")
-	// {
-	// 	std::cout << "\nRun with \"./webserv configuration/webserv.conf\"" << std::endl;
-	// 	return (1);
-	// }
-	mvp();
+	// (void)argc;
+	// (void)argv;
+	if (argc != 2 || std::string(argv[1]) != "configuration/webserv.conf")
+	{
+		std::cout << "\nRun with \"./webserv configuration/webserv.conf\"" << std::endl;
+		return (1);
+	}
+	configParser(argv[1]);
+	// mvp();
 	return (0);
 }
